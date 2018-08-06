76fd74f5b2827d614f6fc4aa5e31a5c70436d0cc
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-08-06 19:02:02 +0100

    [feature] cooufeeds

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const SENDGRID_API_KEY = require('../../config/keys').SENDGRID_API_KEY;
 const validateRegisterInput = require('../../validation/register');
-
+const veriftToken = require('../../config/verifyToken');
 const router = express.Router();
 
 const User = require('../../models/User');
@@ -9,21 +9,22 @@ const User = require('../../models/User');
 // @route   POST api/users/register
 //@desc     Create User
 //@access   Public
-router.post('/register', (req, res) => {
+router.post('/register', veriftToken, (req, res) => {
     const {errors, isValid } = validateRegisterInput(req.body);
-
+    console.log(req.token);
     //Check validation
     if(!isValid){
         return res.status(400).json(errors);
     }
 
-        return res.json('passed')
-    // const lecture ={
-    // course_code: req.body.course_code,
-    // course_title: req.body.course_title,
-    // level: req.body.level,
-    // status: 1,
-    // }
+        return res.json('passed');
+    const user ={
+    matric_no: req.body.matric_no,
+    surname: req.body.surname,
+    firstname: req.body.firstname,
+    email: req.body.email,
+    phone: req.body.phone,
+    }
 
     // new Lecture(lecture).save()
     // .then(lecture => res.json({msg: 'Lecture created', 'data': lecture}))
@@ -36,20 +37,20 @@ router.post('/register', (req, res) => {
 
 // @route   GET api/lecture/
 //@desc     Get list of all lectures
-//@access   Public
-router.get('/', (req, res) => {
-    Lecture.find({status: 1})
-    .then(lectures => {
+//@access   Private
+router.get('/login', veriftToken, (req, res) => {
+    // Lecture.find({status: 1})
+    // .then(lectures => {
         
-        if(lectures.length > 0){
-           return res.json(lectures);
-        }else{
-            return res.json({msg: "No active lectures at this time"});
-        }
-    })
-    .catch(err => {
-        res.status(400).json({msg: "Error getting lectures, please try again."});
-    });
+    //     if(lectures.length > 0){
+    //        return res.json(lectures);
+    //     }else{
+    //         return res.json({msg: "No active lectures at this time"});
+    //     }
+    // })
+    // .catch(err => {
+    //     res.status(400).json({msg: "Error getting lectures, please try again."});
+    // });
  });
 
  // @route   POST api/lecture/cancel
