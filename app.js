18c6333e185c05c313f5903b89eb842ca53e118d
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-08-06 16:52:33 +0100

    [feature] cooufeeds

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json()); 
 
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Authorization');
+
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, PUT');
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+
 //DB config
 const db = require('./config/keys').mongoURI;
 
--- routes/api/lecture.js
+++ routes/api/lecture.js
@@ -1,5 +1,7 @@
 const express = require('express');
-
+const SENDGRID_API_KEY = require('../../config/keys').SENDGRID_API_KEY;
+const fs = require('fs');
+const encode = require('nodejs-base64-encode');
 
 const router = express.Router();
 
@@ -62,5 +64,83 @@ router.post('/cancel', (req, res) => {
         res.status(400).json({msg: "Error cancelling lecture, please try again."});
     });
  });
+
+
+ // @route   POST api/lecture/cancel
+//@desc     Cancel Lecture
+//@access   Private
+router.post('/mail', (req, res) => {
+// console.log(SENDGRID_API_KEY)
+let cont = '';
+let route = (__dirname + '/COVER.docx');
+console.log(route)
+// fs.readFile(route, (err, data) => {
+   
+//     console.log(data)
+//     console.log(err)
+//         if(err){
+//             return res.json(err);
+//         }
+
+//         cont = data;
+
+//     });
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(SENDGRID_API_KEY);
+    // 
+    const msg = {
+      to: 'ekunolaeasybuoy@gmail.com',
+      from: 'test@example.com',
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      attachments: [
+        {
+          content: new Buffer('Some content here').toString('base64'),
+        //   content: cont,
+        //   filename: 'Report.pdf',
+        //   type: 'application/pdf',
+        //   disposition: 'attachment',
+        //   content_id: 'Report '
+        filename: 'some-attachment.docx',
+        type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        disposition: 'attachment',
+        content_id: 'mytext',
+        templateId: 'd-870e182c4c6349e7bf2481d86e459d12',
+        substitutions: {
+        name: 'Some One',
+        date: '2018-08-06',
+        class: 'GNS121',
+        level: '200'
+        },
+        },
+      ],
+    };
+    console.log(msg.attachments[0].content);
+    // console.log(encode.encode('Some base 64 encoded attachment content','base64'))
+   let a = sgMail.send(msg)
+
+       return res.json(a);
+   
+
+
+
+    // console.log(Buffer.from("Hello World").toString('base64'));
+    // let me =  msg.attachments[0].content = Buffer.from(msg.attachments[0].content).toString('base64');
+    // console.log(me)
+//     sgMail.send(msg)
+//     .then((res) => {
+//         console.log('eee')
+//        return res.json('Mail sent successfully')
+//     }
+// )
+//     .catch((err) => res.json(err));
+ 
+    
+});
+
+ 
+
+
 
 module.exports = router;
