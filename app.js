d8a0441a02358587bab80baf05c03f5d6ff839ee
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-08-07 10:11:33 +0100

    [feature] added authorization

--- app.js
+++ app.js
@@ -61,18 +61,18 @@ app.listen(port, () => {
 });
 
 
-// app.use((req, res, next) => {
-//     const error = new Error('Not Found');
-//     error.status = 404;
-//     next(error);
-// });
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
 
-// app.use((error, req, res, next) => {
-//     res.status(error.status || 500);
-//     res.json({
-//         message: error.message
-//     });
-// });
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        message: error.message
+    });
+});
 
 
 module.exports = app;
